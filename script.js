194e15abb2afbbba874ba4d0a82194637815f06e
Moses <88007849+moses0x@users.noreply.github.com>
2023-10-23 10:07:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const OpenModal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    btnsOpenModal[i].addEventListener('click', OpenModal);
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keypress', function (e) {
+    console.log(e.key);
+
+    if (e.key === 'escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    };
+} )
